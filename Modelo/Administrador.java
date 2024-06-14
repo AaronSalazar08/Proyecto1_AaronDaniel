3a3ef7af0dfefa65a15439329e6242efb8eea589
AARON SALAZAR MATA <aaron.salazar.mata@gmail.com>
2024-06-13 21:35:20 -0600

    clase administrador

--- Modelo/Administrador.java
+++ Modelo/Administrador.java
@@ -1,5 +1,76 @@
 package Modelo;
 
 public class Administrador {
+
+    protected String cedula, nombre1, nombre2, apellido1, apellido2, login, clave;
+
+    public Administrador(String cedula, String nombre1, String nombre2, String apellido1, String apellido2,
+            String login, String clave) {
+        this.cedula = cedula;
+        this.nombre1 = nombre1;
+        this.nombre2 = nombre2;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.login = login;
+        this.clave = clave;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre1() {
+        return nombre1;
+    }
+
+    public void setNombre1(String nombre1) {
+        this.nombre1 = nombre1;
+    }
+
+    public String getNombre2() {
+        return nombre2;
+    }
+
+    public void setNombre2(String nombre2) {
+        this.nombre2 = nombre2;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    
     
 }
