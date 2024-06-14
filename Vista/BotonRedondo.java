38cd388d915ecd839fc5b41f5be5697310e97875
AARON SALAZAR MATA <aaron.salazar.mata@gmail.com>
2024-06-14 01:00:29 -0600

    botonRedondo

---
+++ Vista/BotonRedondo.java
@@ -0,0 +1,26 @@
+
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.border.Border;
+import java.awt.Component;
+
+class BotonRedondo implements Border {
+
+    private int radius;
+
+    BotonRedondo(int radius) {
+        this.radius = radius;
+    }
+
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+    }
+
+    public boolean isBorderOpaque() {
+        return true;
+    }
+
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.drawRoundRect(x, y, width-1, height-1, radius, radius);
+    }
+}
