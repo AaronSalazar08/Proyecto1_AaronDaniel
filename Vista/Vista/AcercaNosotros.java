716f23b17a384749a33922dedc0042c6ad5444b3
AARON SALAZAR MATA <aaron.salazar.mata@gmail.com>
2024-06-14 01:29:27 -0600

    Creacion de la ventana AcercaNosotros

---
+++ Vista/Vista/AcercaNosotros.java
@@ -0,0 +1,25 @@
+package Vista;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class AcercaNosotros extends JFrame implements ActionListener {
+
+    public AcercaNosotros (){
+
+
+    }
+
+    public void Elementos (){
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      
+    }
+
+}
